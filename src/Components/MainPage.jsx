148376d89aaed02f3b391097ffded47c987703e6
Shanmuga Priya <shanmugapriya6655@gmail.com>
2025-12-04 18:27:36 +0530

    API updated

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import './MainPage.css'
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_URL || 'https://todo-backend-beta-two.vercel.app';
 
 function MainPage() {
     const [menuOpen, setMenuOpen] = useState(false)
